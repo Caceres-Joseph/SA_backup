50627bb75427c72874965cd4d14a699312b5910c
Joseph <josephccaceres@gmail.com>
2019-11-02 10:42:35 -0600

    asd!

--- almacenamiento/api/controllers/AlmacenamientoController.js
+++ almacenamiento/api/controllers/AlmacenamientoController.js
@@ -459,6 +459,12 @@ exports.aprobar_traduccion_cadena = function (req, res) {
 
 						data.complemento.contenido = contenido;
 						console.log(data);
+
+						res.send({
+							estado: 200,
+							mensaje: data
+						});
+
 					}
 				});
 				/*
